0c2230c9ada89fe36d5d5e06adeb171cde436e81
Colyn <colyn.brown@gmail.com>
2020-02-23 21:23:01 -0700

    update

--- partials/Alerts.vue
+++ partials/Alerts.vue
@@ -2,14 +2,14 @@
   <!--- Alerts -->
   <div class="py-12 sm:py-24 px-3 sm:px-0 border-b">
     <div class="container mx-auto">
-      <div class="md:flex">
-        <div class="md:w-1/3">
+      <div class="sm:flex">
+        <div class="sm:w-1/3">
           <div class="font-bold font-display text-2xl mb-3">Alerts</div>
           <div class="text-gray-600">
             Used for adding notices around the interface
           </div>
         </div>
-        <div class="md:w-2/3">
+        <div class="sm:w-2/3">
           <Alert class="mb-4">
             A simple Default alert. Here is
             <a href="#">an example link</a>.
